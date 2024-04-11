bb48819b5717e61a1cd71d6f847899bcdee3a6ec
Rudrani Mahajan <110345088+m-rudrani@users.noreply.github.com>
2024-04-11 23:58:51 +0530

    Add files via upload

---
+++ static/js/sc.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var backgroundContainer = document.getElementById('background-container');
+    // Change the path to your background image
+    var imagePath = './static/img/bg1.mp4';
+    backgroundContainer.style.backgroundImage = 'url(' + imagePath + ')';
+});
